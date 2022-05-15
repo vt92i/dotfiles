b1efd422acd342340bc31eb44ef32c6ff15f3678
flofi <benidiktusvio@gmail.com>
2022-05-15 13:29:26 +0700

    Add custom path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ set -U fish_greeting
 # Aliases
 source ~/.aliases
 
+# Add custom path
+set PATH ~/.local/bin $PATH
+
 # pfetch configuration
 export PF_INFO="ascii title os kernel uptime pkgs memory palette"
 export PF_ASCII="arch"
