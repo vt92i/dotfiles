7b9ba3397a08942b2dd4ace42ecc89dcc5cabd96
ethan <benidiktusvio@gmail.com>
2022-05-15 16:28:19 +0700

    Update spotify.sh

--- .config/i3/scripts/spotify.sh
+++ .config/i3/scripts/spotify.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
-artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|cut -b 20-|cut -d '"' -f 2|egrep -v ^$|egrep -v array|egrep -v artist`
+# zscroll -l 120 --delay 0.2\
+# 		--match-command "playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'" \
+# 		--update-check true "playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'" &
+# wait
 
-if [ "$artist" != "" ] && [ "title" != "" ]; then
-  echo $artist - $title;
-fi
+current_song=`playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'`
+echo $current_song;
