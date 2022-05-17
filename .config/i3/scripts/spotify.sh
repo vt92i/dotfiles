73f2b00bc860cbc192684340c33bfdbd3e8702cb
flofi <benidiktusvio@gmail.com>
2022-05-17 23:18:14 +0700

    Update spotify.sh

--- .config/i3/scripts/spotify.sh
+++ .config/i3/scripts/spotify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# zscroll -l 120 --delay 0.2\
-# 		--match-command "playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'" \
-# 		--update-check true "playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'" &
-# wait
+zscroll -l 95 --delay 0.3\
+ 		--match-command "playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'" \
+ 		--update-check true "playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'" &
+wait
 
-current_song=`playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'`
-echo $current_song;
+# current_song=`playerctl -p spotify metadata spotify --format '{{ artist }} - {{ title }}'`
+# echo $current_song;
